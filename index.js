43468e1a5115cf7040836af6abe9bc89370f1a49
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-04-13 21:42:02 +0100

    A bit of twiddling re parsing

--- index.js
+++ index.js
@@ -28,14 +28,13 @@ const loop = (jobs, set, output) => {
 const parseLines = lines => lines.split('\n').map(parseLine)
 
 const parseLine = line => {
-  const m1 = line.match(/([a-z])\s=>\s([a-z])/)
-  if (m1) return {
-    id: m1[1],
-    dependency: m1[2]
-  }
-  const m2 = line.match(/([a-z])/)
-  if (m2) return {
-    id: m2[1]
+  const PARSE_TABLE = [
+    [/([a-z]) => ([a-z])/, m => ({ id: m[1], dependency: m[2] })],
+    [/([a-z])/, m => ({ id: m[1] })]
+  ]
+  for (const [r, f] of PARSE_TABLE) {
+    const match = r.exec(line)
+    if (match) return f(match)
   }
   throw new Error(`Failed to parse line, "${line}".`)
 }
