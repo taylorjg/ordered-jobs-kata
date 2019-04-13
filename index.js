c9ebff449c2c78c9490a8dbcf2ed2e5156520553
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-04-13 15:02:25 +0100

    Support a couple of jobs with no dependencies

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const orderJobs = input => {
-  return ''
+const orderJobs = lines => {
+  if (!lines) return ''
+  const jobs = parseLines(lines)
+  return jobs.map(job => job.id).join('')
+}
+
+const parseLines = lines => lines.split('\n').map(parseLine)
+
+const parseLine = line => {
+  const m1 = line.match(/([a-z])\s=>\s([a-z])/)
+  if (m1) return {
+    id: m1[1],
+    dependsOn: m1[2]
+  }
+  const m2 = line.match(/([a-z])/)
+  if (m2) return {
+    id: m2[1]
+  }
+  throw new Error(`Failed to parse line, "${line}".`)
 }
 
 module.exports = orderJobs
--- index.spec.js
+++ index.spec.js
@@ -3,8 +3,22 @@ const orderJobs = require('./index.js')
 
 describe('ordered-jobs-kata tests', () => {
   
-  it('empty input', () => {
+  it('no jobs', () => {
     const actual = orderJobs('')
     expect(actual).to.equal('')
+  })
+
+  it('one job with no dependencies', () => {
+    const actual = orderJobs('a')
+    expect(actual).to.equal('a')
+  })
+
+  it('two jobs with no dependencies', () => {
+    const lines = [
+      'a',
+      'b'
+    ]
+    const actual = orderJobs(lines.join('\n'))
+    expect(actual).to.equal('ab')
   })
 })
